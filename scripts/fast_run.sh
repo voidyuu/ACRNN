8220a02012bde94605ff5f7602dac9b080a37738
voidyuu <zyuxon@gmail.com>
2026-03-24 18:35:55 +0800

    a

--- scripts/fast_run.sh
+++ scripts/fast_run.sh
@@ -1,3 +1,4 @@
+export UV_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
 export HF_ENDPOINT=https://hf-mirror.com
 
 uv run acrnn --target valence --epochs 3 --batch-size 8 --log-every 1
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+export UV_INDEX_URL=https://pypi.tuna.tsinghua.edu.cn/simple
 export HF_ENDPOINT=https://hf-mirror.com
 
 uv run acrnn --target valence --epochs 500 --batch-size 32
